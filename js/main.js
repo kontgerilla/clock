1b9d7627bfac11a5c6f7c40e88101bb472cd1266
Furkan Yıldırım <furkanyidiirm@gmail.com>
2023-07-09 11:30:32 +0300

    version 1

--- js/main.js
+++ js/main.js
@@ -16,8 +16,8 @@ function setDate() {
 
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
-    console.log(secondsDegrees)
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
 
     const mins = now.getMinutes();
     const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
@@ -27,7 +27,11 @@ function setDate() {
     const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
-
-setInterval(setDate, 900);
+const sound = document.getElementById("ticktak")
+function soundplay(){
+    sound.play();
+}
+setInterval(soundplay);
+setInterval(setDate);
 
 setDate();
